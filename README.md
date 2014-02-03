e795c9f5362f98d1293186d2210f27c39c0e125e
Avery Brooks <brooks@radicalmedia.com>
2014-02-03 13:53:50 -0500

    restructure repo, docs

--- README.md
+++ README.md
@@ -1,33 +1,6 @@
 # navigator.getUserMedia()
 
-Reasonably normalization for navigator.getUserMedia. Inspired by [Mike Taylr's **gUM Shield**](https://gist.github.com/f2ac64ed7fc467ccdfe3)
-
-Compressed size: 480 bytes gzipped (778 bytes minified).
-
-## Getting Started
-Download the [production version][min] or the [development version][max].
-
-[min]: https://raw.github.com/rwldrn/navigator.getusermedia/master/dist/navigator.getusermedia.min.js
-[max]: https://raw.github.com/rwldrn/navigator.getusermedia/master/dist/navigator.getusermedia.js
-
-In your web page:
-
-```html
-<!--  You can optionally include Unprefix.js... -->
-<!-- <script src="https://raw.github.com/rwldrn/unprefix.js/master/dist/unprefix.js.min.js"></script> -->
-
-<script src="dist/navigator.getusermedia.min.js"></script>
-<script>
-
-navigator.getUserMedia({ video: true, audio: true }, function( raw, cooked ) {
-
-  // "raw" is the stream as it was received by the host API
-  // "cooked" is the normalized, URL.createObjectURL()-ified
-
-});
-
-</script>
-```
+This is an AMD port of this library, which is intended to be loaded by require.js or similar.  Please visit the [original author](here https://github.com/rwaldron/navigator.getusermedia) for more information and access to the unmodified original source.
 
 ## Supported By These Fine Browsers:
 
@@ -44,3 +17,5 @@ _Also, please don't edit files in the "dist" subdirectory as they are generated 
 ## License
 Copyright (c) 2012 Rick Waldron <waldron.rick@gmail.com>
 Licensed under the MIT license.
+
+AMD port by sprky0 
---
+++ navigator.getusermedia.js
@@ -0,0 +1,60 @@
+define("navigator/getusermedia", [], function() {
+
+	// run a maximum of once
+	if (window.getUserMedia)
+		return;
+	( function(window, navigator) {
+
+			var getUserMedia;
+
+			// 2012-03-08 Inspired by https://gist.github.com/f2ac64ed7fc467ccdfe3
+
+			// If unprefix.js is available, use it.
+			// https://github.com/rwldrn/unprefix.js
+			// Otherwise...
+			if (!window.unprefix) {
+				// Thanks to Mike Taylr for typing this
+				// https://gist.github.com/f2ac64ed7fc467ccdfe3
+				// normalize window.URL
+				if (!window.URL) {
+					window.URL = window.webkitURL || window.msURL || window.oURL;
+				}
+				// normalize navigator.getUserMedia
+				if (!navigator.getUserMedia) {
+					navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+				}
+			}
+
+			getUserMedia = navigator.getUserMedia;
+
+			navigator.getUserMedia = getUserMedia ? function(opts, callback, errback) {
+
+				getUserMedia.call(navigator, opts, function(raw) {
+					var stream;
+
+					// If the stream is raw (ie. Chrome), cook it.
+					if (raw.label && raw.readyState === 1) {
+						stream = window.URL.createObjectURL(raw);
+					}
+
+					// Opera 12.02 does it like this...
+					// Make Firefox Nightly happy...
+					if (raw.createObjectURL || raw.currentTime !== undefined) {
+						stream = raw;
+					}
+
+					// This is non-standard, but feels like a
+					// "nice to have" way to handle the mixed-matched
+					// implementations of stream params.
+					// This will be removed when the implementations
+					// are updated.
+
+					callback(stream, /* non-standard */raw);
+
+				}, errback || function() {});
+
+			} : undefined;
+
+		}( typeof window === "object" && window || this, this.navigator || {}) );
+
+}); 
